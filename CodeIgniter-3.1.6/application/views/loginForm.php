ea3a12ae1c28018933641496d968028c2f863816
Claire <claire.lindblom@gmail.com>
2017-10-24 18:39:22 -0700

    creates a model for users and closes the login form in a div

--- CodeIgniter-3.1.6/application/views/loginForm.php
+++ CodeIgniter-3.1.6/application/views/loginForm.php
@@ -1,8 +1,9 @@
 <?php echo validation_errors(); ?>
-<?php echo form_open('login'); ?>
-<input type="text" name="UserName" placeholder="username">
-<input type="text" name="Password" placeholder="Password">
-</br>
-<input type="submit" name="submit" value="Login">
-
-<? echo form_close(); ?>
+<div>
+	<?php echo form_open('login'); ?>
+	<input type="text" name="UserName" placeholder="username">
+	<input type="text" name="Password" placeholder="Password">
+	</br>
+	<input type="submit" name="submit" value="Login">
+	<? echo form_close(); ?>
+</div>
