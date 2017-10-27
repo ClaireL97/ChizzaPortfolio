9837f4b49da033c558f8c33ddcb4fcbf150c91a9
Claire <claire.lindblom@gmail.com>
2017-10-27 16:38:48 -0700

    adds the propper classes  to synopsis model and controller and makes a basic form

--- CodeIgniter-3.1.6/application/controllers/Synopsis.php
+++ CodeIgniter-3.1.6/application/controllers/Synopsis.php
@@ -1,5 +1,10 @@
 <?php
 class Homepage extends CI_Controller {
 
-	
+	public function synopsis()
+	{
+
+		$this->load->library('form_validation');
+		$error_message = '';
+	}
 }
--- CodeIgniter-3.1.6/application/views/synopsisForm.php
+++ CodeIgniter-3.1.6/application/views/synopsisForm.php
@@ -1,6 +1,6 @@
 <?= validation_errors(); ?>
 <div>
-	<?= form_open('Homepage/synopsis'); ?>
+	<?= form_open('Synopsis/synopsis'); ?>
 	<input type="text" name="synopsis" placeholder="Synopsis">
 	<input type="submit" name="submit" value="Submit">
 	<?= form_close(); ?>
