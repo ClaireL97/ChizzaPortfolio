c6332f57c54d0f422911c27f83dd2a35e526ced0
Claire <claire.lindblom@gmail.com>
2017-11-01 22:29:38 -0700

    adds functions for saving and retrieving about me data from the user info model

--- CodeIgniter-3.1.6/application/models/UserInfo_model.php
+++ CodeIgniter-3.1.6/application/models/UserInfo_model.php
@@ -22,6 +22,24 @@ Class UserInfo_model extends CI_model{
 
 	}
 
+	public function get_about_me($user_id)
+	{
+		$this->db->select('about_me');
+		$this->db->from('user_info');
+		$this->db->where('user_id', $user_id);
+		$about_me = $this->db->get();
+		return $about_me->row()->about_me;
+
+	}
+
+	public function save_about_me($user_id, $about_me)
+	{
+		$data = array(
+			'about_me' => $about_me
+		);
+		$this->db->where('user_id', $user_id);
+		$this->db->update('user_info', $data);
+	}
 
 }
 
