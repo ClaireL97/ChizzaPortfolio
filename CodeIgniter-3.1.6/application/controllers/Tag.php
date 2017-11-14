8f3ed1d686866b4011903aa31f1594754d4de2c6
Claire <claire.lindblom@gmail.com>
2017-11-13 19:23:15 -0800

    creates a function to load all the views including the tag form and creates a function to create tags after form submitted

--- CodeIgniter-3.1.6/application/controllers/Tag.php
+++ CodeIgniter-3.1.6/application/controllers/Tag.php
@@ -1,13 +1,29 @@
 <?php
 class Tag extends CI_Controller {
 
-	public function tag()
+	public function __construct()
 	{
-		
+		parent::__construct();
 	}
 
-	public function create_tag()
+	public function manageTags()
 	{
 
+		$this->load->view("templates/header", array("title"=>"Manage Tags"));
+		$data['footer'] = $this->load->view("templates/footer", NULL, TRUE);
+		$data['tagForm'] = $this->load->view("tagForm", NULL, TRUE);
+		$this->load->view("manageTags", $data);
+
+
+	}
+
+
+	public function create_tag()
+	{	
+		$this->load->model('Tag_model');
+		// create and display form, as you do in about_me or etc
+		// on form submission:
+		// $tagData = array("name"=>form['tag']);
+		//call tag_model->save_tag($tagData)
 	}
 }
