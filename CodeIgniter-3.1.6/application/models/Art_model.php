2baff2002057f3da19b4e4554c2d95ccf7b94791
Claire <claire.lindblom@gmail.com>
2017-11-18 16:29:59 -0800

    adds functions to save updates to art and captions and delete

--- CodeIgniter-3.1.6/application/models/Art_model.php
+++ CodeIgniter-3.1.6/application/models/Art_model.php
@@ -3,9 +3,35 @@ Class Art_model extends CI_model{
 
 	public function save_upload($artUpload)
 	{
-		$this->db->where('user_id', $artUpload['user_id']);
-		$this->db->update('art', $artUpload);
+		$this->db->insert('art', $artUpload);
 	}
+
+	public function delete_art($id)
+	{
+		$data = $this->db->select('*')->from('art')->where('id', $id)->get()->row();
+		$delete = unlink($data->file);
+		if ($delete == true) {
+			// delete art_tags where art_id = $id
+			$this->db->delete('art', array('id' => $id));
+		}
+	}
+
+	public function update_art_data($id, $title, $caption)
+	{
+		$data = array(
+			'title' => $title,
+			'caption' => $caption
+		);
+		$this->db->where('id', $id);
+		$this->db->update('art', $data);
+	}
+
+	public function get_arts()
+	{
+		$arts = $this->db->select('*')->from('art')->get()->result();
+		return $arts;
+	}
+
 
 }
 
