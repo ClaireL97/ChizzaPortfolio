43d293a6f21de67c5984569847c67618931709ad
Claire <claire.lindblom@gmail.com>
2017-11-18 16:28:54 -0800

    creates a view page that displays all art and delete and edit

--- CodeIgniter-3.1.6/application/views/manageArt.php
+++ CodeIgniter-3.1.6/application/views/manageArt.php
@@ -1,3 +1,21 @@
 <p>Upload art</p>
 
 <?= $artUpload_form?>
+
+<?php foreach ($arts as $art) { ?>
+	<?= validation_errors(); ?>
+	<div>
+		<?= form_open("Art/update_art_data"); ?>
+		<input type="text" name="title" value="<?=$art->title?>" placeholder="<?=$art->title?>">
+		<input type="text" name="caption" value="<?=$art->caption?>" placeholder="<?=$art->caption?>">
+		<input type="hidden" value="<?=$art->id?>" name="id">
+		</br>
+		<input type="submit" name="submit" value="Update">
+		<?= form_close(); ?>
+	</div>
+	<?= form_open("Art/delete_art"); ?>
+		<input type="hidden" value="<?=$art->id?>" name="id">
+		<input type="submit" value="Delete">
+	<?= form_close(); ?>
+	<br/>
+<?php } ?>
