58110573cad8921a0bf1ace78297ca2ea72ce120
Claire <claire.lindblom@gmail.com>
2017-11-19 18:01:24 -0800

    adds a tab for Affiliates that redirects to the correct location

--- CodeIgniter-3.1.6/application/views/templates/header.php
+++ CodeIgniter-3.1.6/application/views/templates/header.php
@@ -7,7 +7,18 @@
 		<nav>
 			<a href="/Homepage/index">Home</a>
 			<a href="#">Gallery</a>
+			<?php /*
+
+				//foreach (GalleryModel::getAll() as $gallery) {
+					//<?= a href="gallery/$gallery->id"> $gallery->title ?>
+				}
+
+			*/ ?>
+			<!-- TODO add dropdowns for managing art, tags and gallieries -->
+			<!-- TODO add links to existing pages -->
+			<!-- TODO hide certain links when not logged in -->
 			<a href="/About_me/about_me">About Me</a>
+			<a href="/Affiliate/affiliates">Affiliates</a>
 			<a href="#">Resume</a>
 			<a href="#">Contact</a>
 			<?php if (!isset($_SESSION['logged_in'])) { ?>
