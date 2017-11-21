9edcf4727087b6f92b0919b70372a9390a6b9e92
Claire <claire.lindblom@gmail.com>
2017-11-21 13:02:11 -0800

    adds values to each of the input boxes for the last thing that was entered

--- CodeIgniter-3.1.6/application/views/contactForm.php
+++ CodeIgniter-3.1.6/application/views/contactForm.php
@@ -1,15 +1,16 @@
 <?= validation_errors(); ?>
 <div>
 	<?= form_open('Contact/update_contact_info', array("enctype"=>"multipart/form-data")); ?>
-	<input type="text" name="f_name" placeholder="First Name">
+	<input type="text" name="f_name" value="<?= $contact_info->f_name?>" placeholder="First Name">
 	</br>
-	<input type="text" name="l_name" placeholder="Last Name">
+	<input type="text" name="l_name" value="<?= $contact_info->l_name?>" placeholder="Last Name">
 	</br>
-	<input type="text" name="email" placeholder="Email">
+	<input type="text" name="email" value="<?= $contact_info->email?>" placeholder="Email">
 	</br>
-	<input type="text" name="phone" placeholder="Phone Number">
+	<input type="text" name="phone" value="<?= $contact_info->phone?>" placeholder="Phone Number">
 	</br>
-	<input type="file" name="image">
+	<input type="file" name="profile_pic">
+	</br>
 	<input type="submit" name="submit" value="Submit">
 	<?= form_close(); ?>
 </div>
