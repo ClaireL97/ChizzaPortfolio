b13414081b4000570e6e999afd55e2c1f5c23a17
Claire <claire.lindblom@gmail.com>
2017-11-20 21:03:59 -0800

    adds jquery that only makes the edit and delete form visible when buttons are clicked while logged in

--- CodeIgniter-3.1.6/application/views/manageArt.php
+++ CodeIgniter-3.1.6/application/views/manageArt.php
@@ -4,18 +4,51 @@
 
 <?php foreach ($arts as $art) { ?>
 	<?= validation_errors(); ?>
-	<div>
-		<?= form_open("Art/update_art_data"); ?>
-		<input type="text" name="title" value="<?=$art->title?>" placeholder="<?=$art->title?>">
-		<input type="text" name="caption" value="<?=$art->caption?>" placeholder="<?=$art->caption?>">
-		<input type="hidden" value="<?=$art->id?>" name="id">
-		</br>
-		<input type="submit" name="submit" value="Update">
+	<div class="parent-div">
+		<div class="edit-art" style="display:none">
+			<?= form_open("Art/update_art_data", array('class'=>'edit-art-form')); ?>
+			<input type="text" name="title" value="<?=$art->title?>" placeholder="<?=$art->title?>">
+			<input type="text" name="caption" value="<?=$art->caption?>" placeholder="<?=$art->caption?>">
+			<input type="hidden" value="<?=$art->id?>" name="id">
+			</br>
+			<input type="submit" name="submit" value="Update">
+			<?= form_close(); ?>
+		</div>
+		<button class="edit-art-btn">Edit</button>
+		<?= form_open("Art/delete_art"); ?>
+			<input type="hidden" value="<?=$art->id?>" name="id">
+			<input type="submit" value="Delete">
 		<?= form_close(); ?>
+		<br/>
+		<!-- TODO this only seems to work for one picture -->
+		<div class="show-art">
+			<?= $art->title ?> <br/>
+			<?= $art->caption ?> <br/>
+		</div>
 	</div>
-	<?= form_open("Art/delete_art"); ?>
-		<input type="hidden" value="<?=$art->id?>" name="id">
-		<input type="submit" value="Delete">
-	<?= form_close(); ?>
+	<img src="<?=base_url(array_slice(explode('/', $art->file), -2, 2, true))?>" height="42" width="42">
 	<br/>
 <?php } ?>
+
+
+<?= $footer ?>
+<script>
+// jquery on document load
+$(document).ready(function(){
+	$(document).on('click', '.edit-art-btn', function(e) {
+		$editDiv = $(this).parent();
+		$editDiv.find('.show-art').hide();
+		$editDiv.find('.edit-art').show();
+		$(this).hide();
+	});
+
+	$(document).on('submit', '.edit-art-form', function(e) {
+		$editDiv = $(this).parent();
+		$editDiv.find('.show-art').show();
+		$editDiv.find('.edit-art').hide();
+		$editDiv.find('.edit-art-btn').show();
+	});
+
+});
+
+</script>
