fa305b48f09f1ef37794b868cb574d57099bcdaa
Claire <claire.lindblom@gmail.com>
2017-11-21 13:01:34 -0800

    adds functions to create and update contact information including picture uploading

--- CodeIgniter-3.1.6/application/controllers/Contact.php
+++ CodeIgniter-3.1.6/application/controllers/Contact.php
@@ -9,7 +9,7 @@ class Contact extends CI_Controller {
 	{
 		$this->load->model('UserInfo_model');
 		$this->load->view("templates/header", array("title"=>"Contact Info"));
-		$data['contactInfo'] = $this->UserInfo_model->get_contact_info(1);
+		$data['contact_info'] = $this->UserInfo_model->get_contact_info(1);
 		$data['footer'] = $this->load->view("templates/footer", NULL, TRUE);
 		$data['contactForm'] = $this->load->view("contactForm", $data, TRUE);
 		$this->load->view("contact", $data);
@@ -18,14 +18,30 @@ class Contact extends CI_Controller {
 
 	public function update_contact_info()
 	{
+		
 		$this->load->model('UserInfo_model');
-		$id = $this->input->post('id');
-		$name= $this->input->post('name');
-		$description = $this->input->post('description');
-		$url = $this->input->post('url');
-		$this->UserInfo_model->update_contact_info($id, $name, $description, $url);
+		$user_id = $this->session->userdata('user_id');
+		$config['upload_path'] = '/var/www/chizza_portfolio/CodeIgniter-3.1.6/application/uploads/';
+		$config['allowed_types'] = "*";
+		$this->load->library('upload', $config);
 
-		redirect('/Affiliate/affiliates');
+		if ($this->upload->do_upload('profile_pic'))
+		{
+			$file_data = array('upload_data' => $this->upload->data());
+		}
+		else
+		{
+			$file_data = array(); // empty array for ease-of-processing
+		}
+
+		$f_name= $this->input->post('f_name');
+		$l_name = $this->input->post('l_name');
+		$email = $this->input->post('email');
+		$phone = $this->input->post('phone');
+		$profile_pic = $file_data['upload_data']['full_path'];
+		$this->UserInfo_model->update_contact_info($user_id, $f_name, $l_name, $email, $phone, $profile_pic);
+
+		redirect('/Contact/contact');
 	}
 
 }
--- CodeIgniter-3.1.6/application/models/UserInfo_model.php
+++ CodeIgniter-3.1.6/application/models/UserInfo_model.php
@@ -41,5 +41,37 @@ Class UserInfo_model extends CI_model{
 		$this->db->update('user_info', $data);
 	}
 
+	public function get_contact_info($user_id)
+	{
+		$this->db->select('phone, email, profile_pic, f_name, l_name');
+		$this->db->from('user_info');
+		$this->db->where('user_id', $user_id);
+		$contact_info = $this->db->get();
+		return $contact_info->row();
+
+	}
+
+	public function update_contact_info($user_id, $f_name, $l_name, $email, $phone, $profile_pic)
+	{
+		$data = array(
+			'f_name' => $f_name,
+			'l_name' => $l_name,
+			'email' => $email,
+			'phone' => $phone,
+		);
+		if (!empty($profile_pic)) {
+			$data['profile_pic'] = $profile_pic;
+		}
+		$contact = $this->get_contact_info($user_id);
+		if (file_exists($contact->profile_pic) && !empty($profile_pic))
+		{
+			unlink($contact->profile_pic);
+		}
+		$this->db->where('user_id', $user_id);
+		$this->db->update('user_info', $data);
+
+
+	}
+
 }
 
