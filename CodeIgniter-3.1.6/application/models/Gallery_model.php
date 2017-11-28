41e0ef7b8ed72b3f9a81c60901a5a002a8ce9afb
Claire <claire.lindblom@gmail.com>
2017-11-27 19:31:36 -0800

    added functions to create, edit , get and delete items in the gallery and their respective tags

--- CodeIgniter-3.1.6/application/models/Gallery_model.php
+++ CodeIgniter-3.1.6/application/models/Gallery_model.php
@@ -1,29 +1,47 @@
 <?php
-Class Art_model extends CI_model{
+Class Gallery_model extends CI_model{
 
 	public function getGalleries()
 	{
-	//	// select all galleries
+
+		$galleries = $this->db->select('*')->from('gallery')->get()->result();
 		foreach($galleries as $gallery) {
-			$gallery_tags = $this->db->select('*')->from('gallery_tags')->where('gallery_id', $gallery->id)->get()->result();
+			$gallery_tags = $this->db->select('*')->from('gallery_tags')
+			->join('tag', 'tag.id = gallery_tags.tag_id')->where('gallery_id', $gallery->id)->get()->result();
+			$gallery->tag_ids = array();
+			$gallery->tag_names = array();
 			foreach($gallery_tags as $tag) { // flatten the array
 				$gallery->tag_ids[] = $tag->tag_id;
+				$gallery->tag_names[] = $tag->name;
 			}
 		}
-		// // return $galleries;
+		return $galleries;
 	}
 
-	public function getGalleryTags($id)
+	public function getGalleryArt($gallery_id, $pageNumber)
 	{
-	//	// get all of the tag_id from gallery_tags for the gallery_id=$id
+		$limit = 12;
+		$this->db->select('*')->from('art');
+		$this->db->join('art_tag','art.id = art_tag.id');
+		$this->db->join('gallery','gallery.id = art_tag.tag_id');
+		$this->db->join('gallery', 'gallery.id = gallery_tags.gallery_id');
+		$this->db->where('gallery.id = $gallery_id');
+		$this->db->limit($limit, ($pageNumber * $limit));
+		return $this->db->get()->result();
 	}
 
-	public function updateGallery($title, $id, $tags)
+	public function getGalleryTags($gallery_id)
+	{
+		return $this->db->select('*')->from('gallery_tags')->where('gallery_id', $gallery_id)->get()->result();
+	}
+
+	public function updateGallery($id, $title, $tags)
 	{
 		$data = array(
 			"title" => $title
 		);
-		// // update gallery set title=$title where id = $id
+		$this->db->where('id', $id);
+		$this->db->update('gallery', $data);
 		// update tags
 		$old_tags = $this->getGalleryTags($id);
 		foreach($old_tags as $old_tag)
@@ -32,11 +50,11 @@ Class Art_model extends CI_model{
 		}
 		foreach($tags as $new_tag)
 		{
-			if (!in_array($new_tag, $old_tags)) { // new tag isn't one of the old ones - make it
+			if (!in_array($new_tag, $old_tag_array)) { // new tag isn't one of the old ones - make it
 				$this->db->insert('gallery_tags', array("gallery_id"=>$id, "tag_id"=>$new_tag));
 			} // otherwise, new tag is one of the old tags - don't do anything
 		}
-		foreach ($old_tags as $old_tag) {
+		foreach ($old_tag_array as $old_tag) {
 			if (!in_array($old_tag, $tags)) { // old tag isn't one of the new ones - delete it
 				$this->db->delete('gallery_tags', array("gallery_id"=> $id, "tag_id"=>$old_tag));
 			} // otherwise, old tag is one of the new tags - don't do anything
