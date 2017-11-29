cf4ee197fe1c5fabed76804f25d58aa65862d0b8
Claire <claire.lindblom@gmail.com>
2017-11-28 22:38:23 -0800

    Adds simple view for gallery-art

--- CodeIgniter-3.1.6/application/views/gallery.php
+++ CodeIgniter-3.1.6/application/views/gallery.php
@@ -1,1 +1,21 @@
 <p>Gallery</p>
+
+<?php
+$count = 1;
+foreach($arts as $art) {
+	if ($count-1 % 3 == 0 || $count == 1) { ?>
+		<div class="row">
+	<?php } ?>
+	<div class="col-sm-3">
+		Title: <?=$art->title?> <br/>
+		<img src="<?=base_url(array_slice(explode('/', $art->file), -3, 3, true))?>"> <br/>
+		Caption: <?=$art->caption?>
+	</div>
+	<?php if ($count % 3 == 0 || $count == count($arts)) { ?>
+		</div>
+	<?php } ?>
+	<?php $count++; ?>
+<?php }
+?>
+
+<?=$footer?>
