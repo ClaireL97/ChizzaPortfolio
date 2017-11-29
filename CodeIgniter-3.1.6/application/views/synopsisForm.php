ad460f9b0e4b8a4e2304a41dcf71df764d0862f3
Claire <claire.lindblom@gmail.com>
2017-11-28 22:39:13 -0800

    Adds framework css to synopsis form

--- CodeIgniter-3.1.6/application/views/synopsisForm.php
+++ CodeIgniter-3.1.6/application/views/synopsisForm.php
@@ -1,10 +1,9 @@
 <?= validation_errors(); ?>
 <div>
 	<?= form_open('Homepage/save_synopsis', array("id"=>"synopsis-form")); ?>
-	<textarea name="synopsis" placeholder="Synopsis">
-	<?= $synopsis ?>
-	</textarea>
-	</br>
-	<input type="submit" name="submit" value="Submit">
+	<legend>Synopsis Form</legend>
+	<label for="synopsis">Synopsis</label>
+	<textarea name="synopsis" class="form-control" rows="4" id="synopsis"><?= $synopsis ?></textarea>
+	<center><input type="submit" class="btn btn-primary mt-sm-2" name="submit" value="Submit"></center>
 	<?= form_close(); ?>
 </div>
