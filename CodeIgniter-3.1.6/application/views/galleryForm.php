d1a1875671691804c49bd76b461396575d6e250f
Claire <claire.lindblom@gmail.com>
2017-11-29 13:44:17 -0800

    Adds CSS form styling to create gallery form

--- CodeIgniter-3.1.6/application/views/galleryForm.php
+++ CodeIgniter-3.1.6/application/views/galleryForm.php
@@ -1,14 +1,15 @@
 <?= validation_errors(); ?>
 <div>
 	<?= form_open('gallery/createGallery'); ?>
-	<input type="text" name="title" placeholder="Title">
-	</br>
-	<select multiple name="tags[]">
+	<label for="title">Gallery Name</label>
+	<input type="text" name="title" id="title" class="form-control">
+	<label for="selectTags">Tags Displayed in Gallery</label>
+	<select multiple name="tags[]" class="form-control" id="selectTags">
 		<?php foreach ($tags as $tag) { ?>
 			<option value="<?= $tag->id ?>"> <?= $tag->name ?></option>
 		<?php } ?>
 	</select>
 	</br>
-	<input type="submit" name="submit" value="Create">
+	<input type="submit" name="submit" class="btn btn-primary" value="Create">
 	<?= form_close(); ?>
 </div>
