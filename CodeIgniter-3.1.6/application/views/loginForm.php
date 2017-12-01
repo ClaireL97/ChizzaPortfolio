4287dc2e0487ccd875b81bf140c955ea3652ad4f
Claire <claire.lindblom@gmail.com>
2017-11-30 16:58:23 -0800

    centers header and submit and adds color to the text

--- CodeIgniter-3.1.6/application/views/loginForm.php
+++ CodeIgniter-3.1.6/application/views/loginForm.php
@@ -1,12 +1,12 @@
 <?= validation_errors(); ?>
 <div>
 	<?= form_open('login/login'); ?>
-	<legend>Login</legend>
-	<label for="username">User Name</label>
+	<center><legend class="font-weight-bold emboss text-primary">Login</legend></center>
+	<label class="emboss text-primary" for="username">User Name</label>
 	<input type="text" class="form-control" name="username" id="username">
-	<label for="password">Password</label>
+	<label class="emboss text-primary" for="password">Password</label>
 	<input type="password" class="form-control" name="password" id="password">
-	<input type="submit" class="btn btn-primary mt-sm-2" name="submit" value="Login">
+	<center><input type="submit" class="btn btn-primary mt-sm-2" name="submit" value="Login"></center>
 	<?= form_close(); ?>
 	<?= $error_message ?>
 </div>
