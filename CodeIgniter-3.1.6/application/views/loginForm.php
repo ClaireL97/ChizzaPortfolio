90a8957c0c156851a4d2c7a7a4d6f77a5d4c6b31
Claire <claire.lindblom@gmail.com>
2017-12-01 20:36:23 -0800

    Styles login form to make it look better

--- CodeIgniter-3.1.6/application/views/loginForm.php
+++ CodeIgniter-3.1.6/application/views/loginForm.php
@@ -1,12 +1,17 @@
 <?= validation_errors(); ?>
-<div>
-	<?= form_open('login/login'); ?>
-	<center><legend class="font-weight-bold emboss text-primary">Login</legend></center>
-	<label class="emboss text-primary" for="username">User Name</label>
-	<input required type="text" class="form-control" name="username" id="username">
-	<label class="emboss text-primary" for="password">Password</label>
-	<input required type="password" class="form-control" name="password" id="password">
-	<center><input type="submit" class="btn btn-primary mt-sm-2" name="submit" value="Login"></center>
-	<?= form_close(); ?>
-	<?= $error_message ?>
+<center>
+<div class="row">
+	<div class="col-sm-3"></div>
+	<div class="col-sm-6">
+		<?= form_open('login/login'); ?>
+		<label class="emboss text-primary" for="username">User Name</label>
+		<input required type="text" class="form-control" name="username" id="username">
+		<label class="emboss text-primary" for="password">Password</label>
+		<input required type="password" class="form-control" name="password" id="password">
+		<input type="submit" class="btn btn-primary mt-sm-2" name="submit" value="Login">
+		<?= form_close(); ?>
+		<?= $error_message ?>
+	</div>
+	<div class="col-sm-3"></div>
 </div>
+</center>
