a0ae6f9eadc2d9143f7ad5644122e1b8b919f1f3
Claire <claire.lindblom@gmail.com>
2017-12-03 13:32:17 -0800

    Centers and increases size of synopsis display

--- CodeIgniter-3.1.6/application/views/homePage.php
+++ CodeIgniter-3.1.6/application/views/homePage.php
@@ -1,10 +1,11 @@
-<div id="synopsis">
-	<?= $synopsis ?>
+<center><div id="synopsis">
+	<div style="margin-top: 300px"></div>
+	<h3><?= $synopsis ?></h3>
 	</br>
 	<?php if (isset($_SESSION['logged_in'])) { ?>
 		<button id="show-synopsis-form" class="btn btn-primary" value="Edit">Edit</button>
 	<?php } ?>
-</div>
+</div></center>
 
 <?php if (isset($_SESSION['logged_in'])) { ?>
 	<div id="synopsis-form-div" style="display:none">
