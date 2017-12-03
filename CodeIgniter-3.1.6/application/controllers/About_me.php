3531fcaf20afecc31654e2df7b27995b42575a6b
Claire <claire.lindblom@gmail.com>
2017-12-03 13:43:22 -0800

    adds support to display name on about me page

--- CodeIgniter-3.1.6/application/controllers/About_me.php
+++ CodeIgniter-3.1.6/application/controllers/About_me.php
@@ -10,6 +10,7 @@ class About_me extends CI_Controller {
 	{
 		$this->load->model('UserInfo_model');
 		$this->load->model('Gallery_model');
+		$data['contact_info'] = $this->UserInfo_model->get_contact_info(1);
 		$galleries = $this->Gallery_model->getGalleries();
 		$this->load->view("templates/header", array("title"=>"About Me", "galleries"=>$galleries));
 		$data['about_me'] = $this->UserInfo_model->get_about_me(1);
