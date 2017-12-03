d128805cb8264c274f6fd1f6b475bc8d5930d6c4
Claire <claire.lindblom@gmail.com>
2017-12-03 13:28:09 -0800

    Adds functionality for managing alleries

--- CodeIgniter-3.1.6/application/views/manageGalleries.php
+++ CodeIgniter-3.1.6/application/views/manageGalleries.php
@@ -1,12 +1,38 @@
 <?= $galleryForm ?>
 
 <?php foreach($galleries as $gallery) { ?>
-	Gallery Name: <?= $gallery->title?>
-	Tags: <?= implode($gallery->tag_names,', ')?>
-
+	<?= validation_errors(); ?>
+	<div class="col-sm-3">
+		<?= form_open("gallery/updateGallery"); ?>
+		<label for="title">Gallery Title</label>
+		<input required type="text" name="title" class="form-control" id="title" value="<?=$gallery->title?>" placeholder="<?=$gallery->title?>">
+		<input required type="hidden" value="<?=$gallery->id?>" name="id">
+		<label>Tags</label>
+			<select name="tags[]" multiple class="form-control">
+				<?php foreach($tags as $tag) { ?>
+					<option <?= in_array($tag->id, $gallery->tag_ids) ? 'selected' : '' ?>
+						value="<?=$tag->id?>"> <?=$tag->name?> </option>
+				<?php } ?>
+			</select>
+		<input type="submit" name="submit" class="btn btn-primary" value="Update">
+		<?= form_close(); ?>
+		<?= form_open("gallery/deleteGallery"); ?>
+			<input required type="hidden" value="<?=$gallery->id?>" name="id">
+			<input type="submit" class="delete btn btn-secondary" value="Delete">
+		<?= form_close(); ?>
+	</div>
 	<br/>
-
-
 <?php } ?>
 
 <?=$footer?>
+<script>
+$(document).ready(function() {
+	$(document).on('click', '.delete', function(e) {
+		e.preventDefault();
+		if (confirm('Are you sure you want to delete this?')) {
+			$(this).closest('form').submit();
+		}
+		return false;
+	});
+});
+</script>
