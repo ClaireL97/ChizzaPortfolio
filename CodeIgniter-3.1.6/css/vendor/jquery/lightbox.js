c5b1d9cbbdf2e6085be9276d77c213afa418bd91
Claire <claire.lindblom@gmail.com>
2017-12-03 13:07:03 -0800

    Fixes some jquery errors for the gallery

--- CodeIgniter-3.1.6/css/vendor/jquery/lightbox.js
+++ CodeIgniter-3.1.6/css/vendor/jquery/lightbox.js
@@ -45,13 +45,15 @@ $(document).ready(function() {
     if (n > slides.length) {slideIndex = 1}
     if (n < 1) {slideIndex = slides.length}
     for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";
+      $(slides[i]).hide();
     }
     for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
+      $(dots[i]).removeClass('active');
+     // dots[i].className = dots[i].className.replace(" active", "");
     }
-    slides[slideIndex-1].style.display = "block";
-    dots[slideIndex-1].className += " active";
-    captionText.innerHTML = dots[slideIndex-1].alt;
+    $(slides[slideIndex-1]).show();
+    // dots[slideIndex-1].className += " active";
+    $(dots[slideIndex-1]).addClass('active');
+    captionText.text($(dots[slideIndex-1]).alt);
   }
 });
